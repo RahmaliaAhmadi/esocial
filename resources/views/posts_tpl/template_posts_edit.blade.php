53925572b46e568c0ed73c610eb04be619ee7c71
Amirol Zolkifli <ikursus.com@gmail.com>
2017-05-16 11:23:04 +0800

    Update view with data

--- resources/views/posts_tpl/template_posts_edit.blade.php
+++ resources/views/posts_tpl/template_posts_edit.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Edit Post</div>
+                <div class="panel-heading">Edit Post <?php echo $id; ?></div>
                 <div class="panel-body">
 
                   <form class="form-horizontal" role="form">
